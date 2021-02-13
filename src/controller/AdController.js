0016fc8c9acb0238ec3640602b0909824201eebb
Willian Joaquim Barros Lúcio <willian_jbl@hotmail.com>
2021-02-13 12:03:36 -0300

    added categories

--- src/controller/AdController.js
+++ src/controller/AdController.js
@@ -1,6 +1,19 @@
+import Category from '../model/Category.js';
+import validateRequest from '../validator/validateRequest.js';
+
 const AdController = {
     getCategories: async (req, res) => {
+        const cats = await Category.find();
+        
+        let categories = [];
+        for (let i in cats) {
+            categories.push({
+                ...cats[i]._doc,
+                img: `${process.env.BASE}/assets/images/${cats[i].slug}.png`
+            });
+        }
 
+        res.json(validateRequest(categories));
     },
     addAction: async (req, res) => {
 
